b2592b4cf6c884d50543c3618980339af76124b5
EVAN <evanfwatkins@gmail.com>
2025-10-06 21:01:09 -0500

    Create functions to generate random kits. Dash is not functional

--- tarkov_api.py
+++ tarkov_api.py
@@ -1,18 +1,60 @@
-import dash
-from dash import html, dcc
-from dash.dependencies import Input, Output, State
 import requests
+from pprint import pprint 
 import tarkov_api as api
+import random
 
 def kit_generator():
-    headers = {"Content-Type": "application/json"}
-    all_items_query = """query MyQuery { itemCategories {name}}"""
+    # parse each response into a more formatted list
+    # select a random index from the new list of items
+    
+    helmet_query = """query Helmets {items(name: "Helmet", types: helmet) {name iconLink}}"""
+    helmet = requester(helmet_query, "Helmet")
+    print(f"Helmet: {helmet}")
+    
+    # masks_query = """query Items {items(name: "Mask", types: wearable) {name iconLink}}"""
+    # mask = requester(masks_query, "Mask")
+    # print(f"Mask: {mask}")
 
-    response = requests.post('https://api.tarkov.dev/graphql', headers=headers, json={'query': all_items_query})
-    if response.status_code == 200:
-        return response.json()
+    # headset_query = """query Items {items(name: "Headset", types: wearable) {name iconLink}}"""
+    # headset = requester(headset_query, "Headset")
+    # print(f"Headset: {headset}")
+    
+    # armor_query = """query Armor {items(name: "Armor", types: armor) {name iconLink}}"""
+    # armor = requester(armor_query, "Armor")
+    # print(f"Armor: {armor}")
+
+    # backpack_query = """query Gear {items(name: "Backpack") {name iconLink}}"""
+    # backpack = requester(backpack_query, "Backpack")
+    # print(f"Backpack: {backpack}")
+    
+    # gun_query = """query Weapon {items(types: gun) {name iconLink}}"""
+    # gun = requester(gun_query, "Weapon")
+    # print(f"Weapon: {gun}")
+    
+    # yes_no = ["No", "No"]
+    # customized_weapon = random.choice(yes_no)
+    # print(f"Customized Weapon: {customized_weapon}")
+
+    # grenade_query = """query Weapon {items(types: grenade) {name iconLink}}"""
+    # grenades = requester(grenade_query, "Grenades")
+    # print(f"Grenades: {grenades}")
+
+    
+
+
+def requester(query, type):
+    headers = {"Content-Type": "application/json"}
+    data = requests.post('https://api.tarkov.dev/graphql', headers=headers, json={'query': query})
+    if data.status_code == 200:
+        response = data.json()
+        list_of_items = [(item['name'], item['iconLink']) for item in response['data']['items']]
+        string_with_empty = "empty"
+        list_of_items.append(string_with_empty)
+        # print(list_of_items)
+        random_string = random.choice(list_of_items)
+        return random_string
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
 
-result = run_query()
-print(result)
+if __name__ == "__main__":
+    kit_generator()
